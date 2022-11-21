6c122d26123e8984dba5c59624ce753e45b77286
robroche565 <111483459+robroche565@users.noreply.github.com>
2022-11-21 12:00:04 +0800

    edit
    edited index.php

--- index.php
+++ index.php
@@ -15,7 +15,10 @@
             <tr>
                 <td>ID</td>
                 <td>Subject Code</td>
-                <td>Subject Name</td>
+                <td>Subject Name</td>\
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
                 <td>Action</td>
             </tr>
             <?php 
@@ -24,6 +27,10 @@
                     echo "<td>".$res['Subject_ID']."</td>";
                     echo "<td>".$res['Subject_Code']."</td>";
                     echo "<td>".$res['Subject_Name']."</td>";
+                    echo "<td>".$res['syllabus_id']."</td>";
+                    echo "<td>".$res['syllabus_code']."</td>";
+                    echo "<td>".$res['syllabus_author']."</td>";
+
                     echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a>  |
                             <a href=\"functions/delete.php?id=$res[Subject_ID]\"
                             onClick=\"return confirm('Are you sure you want to delete this?');\">Delete</a></td>";
